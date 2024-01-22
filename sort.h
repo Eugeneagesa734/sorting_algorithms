5b1b155790f99948bcb54c0770dc7ad45fba110d
Eugene Agesa <eugeneagesa734@gmail.com>
2024-01-22 03:44:07 -0800

    added  a prototlpe for bubble sort

--- sort.h
+++ sort.h
@@ -34,6 +34,7 @@ typedef struct listint_s
 } listint_t;
 
 /* Function to act as a print helper.*/
+void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
